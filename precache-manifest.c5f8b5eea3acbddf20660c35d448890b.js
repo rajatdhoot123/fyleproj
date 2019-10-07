1c362a748bcebd2a839aabe465803cbc6e39d205
Rajat <rajatdhoot123@gmail.com>
2019-10-07 23:47:14 +0530

    Updates

---
+++ precache-manifest.c5f8b5eea3acbddf20660c35d448890b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3fc794f6b269e4cb18c63bb8a0e971",
+    "url": "/fyleproj/index.html"
+  },
+  {
+    "revision": "38784102cf64ec62afb7",
+    "url": "/fyleproj/static/css/main.bc619892.chunk.css"
+  },
+  {
+    "revision": "ec46e5a19ab73c5e414b",
+    "url": "/fyleproj/static/js/2.d52160f9.chunk.js"
+  },
+  {
+    "revision": "38784102cf64ec62afb7",
+    "url": "/fyleproj/static/js/main.d65f2763.chunk.js"
+  },
+  {
+    "revision": "64239577dc22342c6238",
+    "url": "/fyleproj/static/js/runtime-main.6be90dd3.js"
+  }
+]);
